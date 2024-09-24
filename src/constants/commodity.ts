efbcfba9879bbd271549e3c2e5e3d23c9d4567af
MurodillaErgashev03 <ergashevmurodilla27@gmail.com>
2024-09-24 13:44:07 +0500

    finish

--- src/constants/commodity.ts
+++ src/constants/commodity.ts
@@ -1,98 +1,99 @@
 import comImg1 from "./../assets/images/pb1.png";
 import comImg2 from "../assets/images/pb2.png";
 import comImg3 from "../assets/images/pb3.png";
- 
-interface Commodity {
-    imageUrl: string;
-    name: string;
-    country: string;
-    description: string;
-    id: number;
-}
+import logo from "../assets/images/medollogo.png";
 
+interface Commodity {
+  imageUrl: string;
+  name: string;
+  country: string;
+  description: string;
+  id: number;
+}
+export { logo };
 export const commodity: Commodity[] = [
-    {
-        imageUrl: comImg1,
-        description: "Интегрированная система Vitros 5600",
-        name: "Horiba",
-        country: "Франция",
-        id: 0,
-    },
-    {
-        imageUrl: comImg2,
-        description: "Иммунохимический анализатор Ortho Vitros 3600",
-        name: "Horiba",
-        country: "Франция",
-        id: 1,
-    },
-    {
-        imageUrl: comImg3,
-        description: "Биохимическая система VITROS XT 3400",
-        name: "Horiba",
-        country: "Франция",
-        id: 2,
-    },
-    {
-        imageUrl: comImg2,
-        description: "Биохимический анализатор System Pentra C200",
-        name: "Horiba",
-        country: "Франция",
-        id: 3,
-    },
-    {
-        imageUrl: comImg3,
-        description: "Биохимический анализатор System Pentra C400",
-        name: "Horiba",
-        country: "Франция",
-        id: 4,
-    },
-    {
-        imageUrl: comImg1,
-        description: "Интегрированная система Vitros 5600",
-        name: "Horiba",
-        country: "Франция",
-        id: 5,
-    },
-    {
-        imageUrl: comImg1,
-        description: "Интегрированная система Vitros 5600",
-        name: "Horiba",
-        country: "Франция",
-        id: 0,
-    },
-    {
-        imageUrl: comImg2,
-        description: "Иммунохимический анализатор Ortho Vitros 3600",
-        name: "Horiba",
-        country: "Франция",
-        id: 1,
-    },
-    {
-        imageUrl: comImg3,
-        description: "Биохимическая система VITROS XT 3400",
-        name: "Horiba",
-        country: "Франция",
-        id: 2,
-    },
-    {
-        imageUrl: comImg2,
-        description: "Биохимический анализатор System Pentra C200",
-        name: "Horiba",
-        country: "Франция",
-        id: 3,
-    },
-    {
-        imageUrl: comImg3,
-        description: "Биохимический анализатор System Pentra C400",
-        name: "Horiba",
-        country: "Франция",
-        id: 4,
-    },
-    {
-        imageUrl: comImg1,
-        description: "Интегрированная система Vitros 5600",
-        name: "Horiba",
-        country: "Франция",
-        id: 5,
-    },
+  {
+    imageUrl: comImg1,
+    description: "Интегрированная система Vitros 5600",
+    name: "Horiba",
+    country: "Франция",
+    id: 0,
+  },
+  {
+    imageUrl: comImg2,
+    description: "Иммунохимический анализатор Ortho Vitros 3600",
+    name: "Horiba",
+    country: "Франция",
+    id: 1,
+  },
+  {
+    imageUrl: comImg3,
+    description: "Биохимическая система VITROS XT 3400",
+    name: "Horiba",
+    country: "Франция",
+    id: 2,
+  },
+  {
+    imageUrl: comImg2,
+    description: "Биохимический анализатор System Pentra C200",
+    name: "Horiba",
+    country: "Франция",
+    id: 3,
+  },
+  {
+    imageUrl: comImg3,
+    description: "Биохимический анализатор System Pentra C400",
+    name: "Horiba",
+    country: "Франция",
+    id: 4,
+  },
+  {
+    imageUrl: comImg1,
+    description: "Интегрированная система Vitros 5600",
+    name: "Horiba",
+    country: "Франция",
+    id: 5,
+  },
+  {
+    imageUrl: comImg1,
+    description: "Интегрированная система Vitros 5600",
+    name: "Horiba",
+    country: "Франция",
+    id: 0,
+  },
+  {
+    imageUrl: comImg2,
+    description: "Иммунохимический анализатор Ortho Vitros 3600",
+    name: "Horiba",
+    country: "Франция",
+    id: 1,
+  },
+  {
+    imageUrl: comImg3,
+    description: "Биохимическая система VITROS XT 3400",
+    name: "Horiba",
+    country: "Франция",
+    id: 2,
+  },
+  {
+    imageUrl: comImg2,
+    description: "Биохимический анализатор System Pentra C200",
+    name: "Horiba",
+    country: "Франция",
+    id: 3,
+  },
+  {
+    imageUrl: comImg3,
+    description: "Биохимический анализатор System Pentra C400",
+    name: "Horiba",
+    country: "Франция",
+    id: 4,
+  },
+  {
+    imageUrl: comImg1,
+    description: "Интегрированная система Vitros 5600",
+    name: "Horiba",
+    country: "Франция",
+    id: 5,
+  },
 ];
